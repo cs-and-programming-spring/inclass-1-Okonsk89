10bcaa66ef9e1724c0385a74ce7fac87b7255db6
Nevin O'Konski <okonsk89@rowan.edu>
2019-01-30 10:39:22 -0500

    Made a miles to km, m, cm and mm converter. Made a circumference calculator.

--- Source.cpp
+++ Source.cpp
@@ -18,25 +18,55 @@ const  float PI = 3.14159;
 int main()
 {
 
-	float radius, area, circumference;
-	
+	float miles, kilometers, meters, centimeters, milimeters;
+
 	//Display purpose of the program
-	cout << " ******************************************"<<endl;
-	cout << " This program takes in radius to compute area of circle" <<endl;
+	cout << " ******************************************" << endl;
+	cout << " Converts Miles to Kilometers, meters, cenimeters and milimeters" << endl;
 	cout << " ******************************************\n\n";
-	
+
+	//Get user input
+	cout << "Please input distance in miles: ";
+	cin >> miles;
+
+	//Convert Miles to kilometerts, meters, centimeters and milimeters
+	kilometers = miles * 1.6;
+	meters = kilometers * 1000;
+	centimeters = meters * 100;
+	milimeters = meters * 1000;
+
+	//Display kilometers
+	cout << " The distance in kilometers is " << kilometers << "\n\n";
+	cout << " The distance in meters is " << meters << "\n\n";
+	cout << " The distance in centimeters is " << centimeters << "\n\n";
+	cout << " The distance in milimeters is " << milimeters << "\n\n";
+	cout << " Program has ended" << endl;
+
+	system("pause");
+
+	float radius, circumference;
+
+	//Display purpose of the program
+	cout << " ******************************************" << endl;
+	cout << " This program takes in radius to compute circumference of circle"<< endl;
+	cout << " ******************************************\n\n";
+
 	//Get user input
 	cout << "Please input radius: ";
 	cin >> radius;
-	
-	//Compute area to PI*radius*radius
-	area = PI* radius *radius; 
-	
-	//Display the area 
-	cout << " The area is = " << area << "\n\n";
-	cout << "Program has ended"<<endl;
+
+	//Compute circumference to 2 * PI * radius
+	circumference = 2 * PI * radius;
+
+	//Display the circumference 
+	cout << " The circumference is = " << circumference << "\n\n";
+	cout << "Program has ended" << endl;
 
 	system("pause");
+
 	
+
+
+
 	return 0;
 }
